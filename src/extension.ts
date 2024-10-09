548b521e81b27706c539005bd7a80b15f0750ea5
k_lar <zan.sprogar@gmail.com>
2024-10-09 19:39:15 +0200

    Added binary download button

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,29 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
+import * as https from 'https';
+import * as fs from 'fs';
 const { exec } = require('child_process');
+
+let DYNOMARK_URL: string;
+let DYNOMARK_EXECUTABLE: string;
+
+// check OS and build url accordingly
+switch (process.platform) {
+    case 'win32':
+        DYNOMARK_URL = 'https://github.com/k-lar/dynomark/releases/latest/download/dynomark.exe';
+        DYNOMARK_EXECUTABLE = 'dynomark.exe';
+        break;
+    case 'linux':
+        DYNOMARK_URL = 'https://github.com/k-lar/dynomark/releases/latest/download/dynomark-linux';
+        DYNOMARK_EXECUTABLE = 'dynomark-linux';
+        break;
+    case 'darwin':
+        DYNOMARK_URL = 'https://github.com/k-lar/dynomark/releases/latest/download/dynomark-macos';
+        DYNOMARK_EXECUTABLE = 'dynomark-macos';
+        break;
+    default:
+        vscode.window.showInformationMessage('Unsupported platform detected. Please download Dynomark manually and add it to your PATH environment variable.');
+}
 
 class DynomarkContentProvider implements vscode.TextDocumentContentProvider {
     private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
@@ -32,87 +55,56 @@ export function activate(context: vscode.ExtensionContext) {
         const cursorPosition = editor.selection.active;
         const text = document.getText();
 
-        // Determine the command to check for dynomark
-        const checkCommand = process.platform === 'win32' ? 'where dynomark' : 'which dynomark';
+        checkDynomarkAvailability().then(dynomarkPath => {
+            // Find the dynomark block where the cursor is located
+            const { content: dynomarkBlock, endPosition } = findDynomarkBlockAtPosition(text, cursorPosition);
 
-        // Check if dynomark is available
-        exec(checkCommand, (error: any, stdout: string, stderr: string) => {
-            if (error) {
-                vscode.window.showErrorMessage('Dynomark is not available on this system.', 'Download Dynomark').then(selection => {
-                    if (selection === 'Download Dynomark') {
-                        vscode.env.openExternal(vscode.Uri.parse('https://github.com/k-lar/dynomark/releases/latest'));
-                    }
-                });
+            if (!dynomarkBlock) {
+                vscode.window.showInformationMessage('Cursor is not inside a dynomark code block.');
                 return;
             }
-        });
-    
-        // Find the dynomark block where the cursor is located
-        const { content: dynomarkBlock, endPosition } = findDynomarkBlockAtPosition(text, cursorPosition);
-    
-        if (!dynomarkBlock) {
-            vscode.window.showInformationMessage('Cursor is not inside a dynomark code block.');
-            return;
-        }
-    
-        const provider = new DynomarkContentProvider();
-        vscode.workspace.registerTextDocumentContentProvider('dynomark-results', provider);
 
-        const currentFileDirectory = path.dirname(document.fileName);
-        const command = `dynomark --query '${dynomarkBlock}'`;
-    
-        exec(command, { cwd: currentFileDirectory }, (error: { message: any; }, stdout: any, stderr: any) => {
-            if (error) {
-                vscode.window.showErrorMessage(`Error running command: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                vscode.window.showWarningMessage(`stderr: ${stderr}`);
-            }
+            const provider = new DynomarkContentProvider();
+            vscode.workspace.registerTextDocumentContentProvider('dynomark-results', provider);
 
-            provider.updateContent(stdout.trim());
+            const currentFileDirectory = path.dirname(document.fileName);
+            const command = `${dynomarkPath} --query '${dynomarkBlock}'`;
 
-            const virtualDocUri = vscode.Uri.parse('dynomark-results:results.md');
-            if (endPosition) {
-                vscode.commands.executeCommand('editor.action.peekLocations', editor.document.uri, endPosition, [
-                    new vscode.Location(virtualDocUri, new vscode.Position(0, 0))
-                ]).then(() => {
-                    console.log('Peek command executed.');
-                }, (err) => {
-                    vscode.window.showErrorMessage(`Error executing peek command: ${err}`);
-                });
-            } else {
-                vscode.window.showErrorMessage('End position is undefined.');
-            }
-        });
-    });
-
-    vscode.languages.registerCodeActionsProvider('markdown', {
-        provideCodeActions(document, range, context, token) {
-            const markdownText = document.getText();
-            const regex = /```dynomark([\s\S]*?)```/g;
-
-            // FIXME: Yeah this doesn't work. Context menu item is always available, even when the cursor is not inside a dynomark block.
-            let match;
-            while ((match = regex.exec(markdownText)) !== null) {
-                const blockStartIndex = match.index;
-                const blockEndIndex = regex.lastIndex;
-
-                const startLine = markdownText.slice(0, blockStartIndex).split('\n').length - 1;
-                const endLine = markdownText.slice(0, blockEndIndex).split('\n').length - 1;
-
-                if (range.start.line >= startLine && range.end.line <= endLine) {
-                    return [
-                        {
-                            title: 'Run block with dynomark',
-                            command: 'vscode-dynomark.runDynomarkBlock'
-                        }
-                    ];
+            exec(command, { cwd: currentFileDirectory }, (error: { message: any; }, stdout: any, stderr: any) => {
+                if (error) {
+                    vscode.window.showErrorMessage(`Error running command: ${error.message}`);
+                    return;
                 }
-            }
+                if (stderr) {
+                    vscode.window.showWarningMessage(`stderr: ${stderr}`);
+                }
 
-            return [];
-        }
+                provider.updateContent(stdout.trim());
+
+                const virtualDocUri = vscode.Uri.parse('dynomark-results:results.md');
+                if (endPosition) {
+                    vscode.commands.executeCommand('editor.action.peekLocations', editor.document.uri, endPosition, [
+                        new vscode.Location(virtualDocUri, new vscode.Position(0, 0))
+                    ]).then(() => {
+                        console.log('Peek command executed.');
+                    }, (err) => {
+                        vscode.window.showErrorMessage(`Error executing peek command: ${err}`);
+                    });
+                } else {
+                    vscode.window.showErrorMessage('End position is undefined.');
+                }
+            });
+        }).catch(() => {
+            vscode.window.showErrorMessage('Dynomark is not available on this system.', 'Download Dynomark').then(selection => {
+                if (selection === 'Download Dynomark') {
+                    downloadDynomark().then(dynomarkPath => {
+                        vscode.window.showInformationMessage(`Dynomark downloaded to ${dynomarkPath}`);
+                    }).catch(err => {
+                        vscode.window.showErrorMessage(`Failed to download Dynomark: ${err}`);
+                    });
+                }
+            });
+        });
     });
 
     let disposableCompileDocument = vscode.commands.registerCommand('vscode-dynomark.compileDocument', () => {
@@ -125,27 +117,14 @@ export function activate(context: vscode.ExtensionContext) {
         const text = document.getText();
 
         // Determine the command to check for dynomark
-        const checkCommand = process.platform === 'win32' ? 'where dynomark' : 'which dynomark';
-
-        // Check if dynomark is available
-        exec(checkCommand, (error: any, stdout: string, stderr: string) => {
-            if (error) {
-                vscode.window.showErrorMessage('Dynomark is not available on this system.', 'Download Dynomark').then(selection => {
-                    if (selection === 'Download Dynomark') {
-                        vscode.env.openExternal(vscode.Uri.parse('https://github.com/k-lar/dynomark/releases/latest'));
-                    }
-                });
-                return;
-            }
-
+        checkDynomarkAvailability().then(dynomarkPath => {
             // Extract all dynomark blocks
             const dynomarkBlocks = extractDynomarkCodeBlocks(text);
             let modifiedText = text;
             let promises = dynomarkBlocks.map(block => {
                 return new Promise<void>((resolve, reject) => {
-                    const command = `dynomark --query '${block}'`;
-                    const currentFileDirectory = path.dirname(document.fileName);
-                    exec(command, { cwd: currentFileDirectory }, (error: { message: any; }, stdout: any, stderr: any) => {
+                    const command = `${dynomarkPath} --query '${block}'`;
+                    exec(command, (error: { message: any; }, stdout: any, stderr: any) => {
                         if (error) {
                             reject(`Error running command: ${error.message}`);
                             return;
@@ -166,6 +145,16 @@ export function activate(context: vscode.ExtensionContext) {
                 });
             }).catch(err => {
                 vscode.window.showErrorMessage(err);
+            });
+        }).catch(() => {
+            vscode.window.showErrorMessage('Dynomark is not available on this system.', 'Download Dynomark').then(selection => {
+                if (selection === 'Download Dynomark') {
+                    downloadDynomark().then(dynomarkPath => {
+                        vscode.window.showInformationMessage(`Dynomark downloaded to ${dynomarkPath}`);
+                    }).catch(err => {
+                        vscode.window.showErrorMessage(`Failed to download Dynomark: ${err}`);
+                    });
+                }
             });
         });
     });
@@ -219,6 +208,64 @@ function findDynomarkBlockAtPosition(markdownText: string, position: vscode.Posi
     }
 
     return { content: null, endPosition: null };  // Return null if no block found
+}
+
+function checkDynomarkAvailability(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const checkCommand = process.platform === 'win32' ? 'where dynomark' : 'which dynomark';
+        exec(checkCommand, (error: any, stdout: string, stderr: string) => {
+            if (error) {
+                // Dynomark not found in PATH, check if we have a downloaded version
+                const dynomarkPath = path.join(__dirname, DYNOMARK_EXECUTABLE);
+                if (fs.existsSync(dynomarkPath)) {
+                    resolve(dynomarkPath);
+                } else {
+                    reject();
+                }
+            } else {
+                resolve('dynomark');
+            }
+        });
+    });
+}
+
+function downloadDynomark(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const dynomarkPath = path.join(__dirname, DYNOMARK_EXECUTABLE);
+        const file = fs.createWriteStream(dynomarkPath);
+
+        const download = (url: string) => {
+            https.get(url, response => {
+                if (response.statusCode === 200) {
+                    response.pipe(file);
+                    file.on('finish', () => {
+                        file.close(() => {
+                            if (process.platform !== 'win32') {
+                                fs.chmod(dynomarkPath, '755', (err) => {
+                                    if (err) {
+                                        reject(`Failed to set execute permissions: ${err.message}`);
+                                    } else {
+                                        resolve(dynomarkPath);
+                                    }
+                                });
+                            } else {
+                                resolve(dynomarkPath);
+                            }
+                        });
+                    });
+                } else if (response.statusCode === 302 && response.headers.location) {
+                    // Follow the redirection
+                    download(response.headers.location);
+                } else {
+                    reject(`Failed to download Dynomark: ${response.statusCode}`);
+                }
+            }).on('error', err => {
+                fs.unlink(dynomarkPath, () => reject(err.message));
+            });
+        };
+
+        download(DYNOMARK_URL);
+    });
 }
 
 export function deactivate() {}
